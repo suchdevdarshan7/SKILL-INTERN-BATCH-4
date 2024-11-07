98fd83c28f73a4c0e88ace5496929fba55aab5cb
suchdevdarshan7 <suchdev7@gmail.com>
2024-11-07 18:41:17 +0530

    Updated day 1 js day 9 class

---
+++ day9-js/app.js
@@ -0,0 +1,34 @@
+//! document
+
+// a = 10;
+// b = true || false;
+// c = "String"; // ' ' ``
+// d = undefined;
+// e = null;
+// f = 10n;
+
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+// console.log(typeof e)
+// console.log(typeof f)
+
+// body = document.body;
+
+// body.style.backgroundColor = "green"
+
+//! arthimetic operators or mathematical operators:
+// a = 10;
+// b = 20;
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+
+// console.log(11 % 2) //only for integers
+// console.log(11 / 2) //convert to decimal
+
+
+console.log(10 ** 4)
